5db5a24e8fb136439fc2772628830570082e581e
s183214 <45406081+s183214@users.noreply.github.com>
2020-01-13 17:48:43 +0100

    AAA

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { SpeechRecognition } from '@ionic-native/speech-recognition/ngx';
+import { NavController } from '@ionic/angular';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +8,52 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  bgcolor: string = 'white';
+
+  constructor(public navCtrl: NavController, private speechRecognition: SpeechRecognition) {
+
+  }
+
+  ngOnInit() {
+
+    // Get the list of supported languages
+    this.speechRecognition.getSupportedLanguages()
+      .then(
+        (languages: Array<string>) => console.log(languages),
+        (error) => console.log(error)
+      )
+
+
+
+    this.speechRecognition.hasPermission()
+      .then((hasPermission: boolean) => {
+
+        if (!hasPermission) {
+          this.speechRecognition.requestPermission()
+            .then(
+              () => console.log('Granted'),
+              () => console.log('Denied')
+            )
+        }
+
+      });
+
+  }
+
+  start() {
+    let options = {
+      language: 'da-DK'
+    }
+
+    this.speechRecognition.startListening(options)
+      .subscribe(
+        (matches: Array<string>) => {
+          console.log(matches);
+          this.bgcolor = matches[0];
+        },
+        (onerror) => console.log('error:', onerror)
+      )
+
+  }
 
 }
